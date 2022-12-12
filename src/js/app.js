76956fb2f779c7a2a4d12dcc1a58641467150e97
ArkadiyLebedev <bepluvdt@gmail.com>
2022-12-12 19:21:11 +0300

    finish

--- src/js/app.js
+++ src/js/app.js
@@ -1,71 +1,126 @@
-import {hideSubMenu} from "./modules/functions.js";
+import {
+    hideSubMenu
+} from "./modules/functions.js";
 
 
 
 const filterItems = document.querySelectorAll("[data-sub='submenu']"),
-      body =  document.querySelector("body"),
-      btnShema = document.querySelectorAll(".header__btn-shema");
-     
-    document.addEventListener('click', e => {
-        let target = e.target;
-        filterItems.forEach(el => {
-            let itsMenu = target == el || el.contains(target);
-            let menuIsActive = el.classList.contains('--active');
-                if (!itsMenu && menuIsActive) {
-                    hideSubMenu(filterItems)
-                    // el.classList.toggle("--active")
-            }
-        })        
-      })
-    
+    body = document.querySelector("body"),
+    gamburger = document.querySelector(".gumburger"),
+    mobileMenu = document.querySelector(".mobile-menu"),
+    mobileBtnClose = document.querySelector(".mobile-close"),
+    // btnShema = document.querySelectorAll(".header__btn-shema"),
+    btnShemaMobile = document.querySelectorAll("[data-shema]"),
+    settingGalery = document.querySelectorAll("[data-gallery] .sort__item-text"),
+    settingCategory = document.querySelectorAll("[data-category] .sort__item-text");
+
+    const hideElementsAll = (elemArray) => {
+        elemArray.forEach(el => {
+            el.classList.remove('--active');
+        })
+    }
 
 
-filterItems.forEach(el => {
-    el.addEventListener("click", (e) => {
-        
-        
-        for (let elem of el.lastElementChild.children) {
-                
-            if (elem != e.target || el.lastChild != e.target) {
-                console.log(e.target)
-                console.log(elem)
-                el.classList.toggle("--active")
-            } else {
-                console.log(5456)
-                   if (el.classList.contains("--active")) {
-                            el.classList.remove("--active")
-                        } else {
-                            hideSubMenu(filterItems)
-                        el.classList.add("--active");
-                        }
-                        }
-    
-            
+const tabSetting = (tabArray) => {
+    tabArray.forEach(el => {
+    el.addEventListener("click", () => {
+        if (!el.classList.contains("--active")) {
+            hideElementsAll(tabArray);
+            el.classList.add("--active");
+           
         }
+    })
+})
+}
 
-        
-     
-        //          if (el.classList.contains("--active")) {
-        //     el.classList.remove("--active")
-        // } else {
-        //     hideSubMenu(filterItems)
-        // el.classList.add("--active");
-        // }
+
+tabSetting(settingGalery);
+tabSetting(settingCategory);
+
+
+const mobileMenuHide = () => {
+    mobileMenu.classList.remove("--mobile-show")
+}
+
+gamburger.addEventListener("click", () => {
+    mobileMenu.classList.add("--mobile-show")
+})
+
+mobileBtnClose.addEventListener("click", () => {
+    mobileMenuHide()
+})
+
+
+document.addEventListener('click', e => {
+    let target = e.target;    
+    filterItems.forEach(el => {
+        let parentTag = "[data-parent]";
+        let itsMenu = target == el || el.closest(parentTag).contains(target);
+        let menuIsActive = el.closest(parentTag).classList.contains('--active');
+        if (!itsMenu && menuIsActive) {
+            console.log(55555)
+            hideSubMenu(filterItems)
+        }
     })
 })
 
 
+filterItems.forEach(el => {
+    el.addEventListener("click", (e) => {
+        let parentDiv = el.closest("[data-parent]")
 
-    btnShema.forEach(el => {
-        el.addEventListener("click", () => {
-            if (!el.classList.contains("--active")) {
-                hideSubMenu(btnShema)
-                el.classList.add("--active");
-                body.removeAttribute("class");
-                body.classList.add(el.getAttribute('data-shema'));
-            }
-        })
+        if (parentDiv.classList.contains("--active")) {
+            parentDiv.classList.remove("--active")
+        } else {
+            hideSubMenu(filterItems)
+            parentDiv.classList.add("--active");
+        }
+    })
 })
 
+// btnShema.forEach(el => {
+//     el.addEventListener("click", () => {
+//         if (!el.classList.contains("--active")) {
+//             hideElementsAll(btnShema);
+//             el.classList.add("--active");
+//             body.removeAttribute("class");
+//             body.classList.add(el.getAttribute('data-shema'));
+//         }
+//     })
+// })
 
-    
+
+btnShemaMobile.forEach(el => {
+    el.addEventListener("click", () => {
+        if (!el.classList.contains("--active")) {
+            hideElementsAll(btnShemaMobile);
+            el.classList.add("--active");
+            body.removeAttribute("class");
+            body.classList.add(el.getAttribute('data-shema'));
+        }
+    })
+})
+
+// копка показать фильтры
+
+const btnFilterGallery = document.querySelector("[data-btn='filter-gallery']");
+const filterGallery = document.querySelector("[data-filter='gallery']");
+
+const btnFilterCategory = document.querySelector("[data-btn='filter-catagory']");
+const filterCategory = document.querySelector("[data-filter='catagory']");
+
+
+const filterToggle = (btn, filter) => {
+    btn.addEventListener("click", () => {
+        if (!btn.classList.contains("--active")) {
+            btn.classList.add("--active")
+            filter.classList.add("filter__wrapper--show")
+        } else {
+            btn.classList.remove("--active")
+            filter.classList.remove("filter__wrapper--show")
+        }    
+    })
+}
+
+filterToggle(btnFilterGallery, filterGallery)
+filterToggle(btnFilterCategory, filterCategory)
--- src/js/modules/functions.js
+++ src/js/modules/functions.js
@@ -1,6 +1,7 @@
 /* Проверка поддержки webp, добавление класса webp или no-webp для HTML */
 export const hideSubMenu = (elemArray) => {
     elemArray.forEach(el => {
-        el.classList.remove('--active');
+        el.closest("[data-parent]").classList.remove('--active');
     })
 }
+
